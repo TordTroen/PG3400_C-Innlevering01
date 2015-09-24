c192fec3082d3633c5a57e0b7dbf64ba184db9fd
Tord <assessedgrub@gmail.com>
2015-09-24 07:18:28 -0700

    Cleaned up the code

--- Sorting.c
+++ Sorting.c
@@ -1,40 +1,56 @@
 #include<stdio.h>
 
-void printList(int list[200000], int length)
-{
-	for (int i = 0; i < length; i ++)
-	{
-		printf("List[%d]=%d\n", i, list[i]);
-	}
-}
+// Function declarations
+void printList(int list[], int length);
+void readFileContent(char *fileName, int *length, int *list);
+void bubbleSort(int *list, int length);
 
 int main(int argc, char *argv[])
 {
-	FILE* f = fopen ("1.txt" , "r");
+	int length = 0; // Length of the list
+	int list[200000]; // The list to be sorted
+
+	// Read numbers from file to list
+	readFileContent("TestFile.txt", &length, list);
+	
+	// TODO: take input and chose sorting algorithm
+
+	// Sort the list
+	bubbleSort(list, length);
+
+	// Print the list
+	printList(list, length);
+}
+
+void readFileContent(char *fileName, int *length, int *list)
+{
+	
+	FILE* f = fopen (fileName , "r");
 	int i = 0;
 	int size = 0;
-	int list[200000];
 	while (!feof (f)) // while not EndOfFile
 	{
 		fscanf (f, "%d", &i);
 		list[size] = i;
 		if (feof(f)) break;
-		printf("Read[%d]=%d\n", size, i);
+		//printf("Read[%d]=%d\n", size, i);
 		size ++;
 	}
-
-	printf("\n");
-	printf("Size: %d\n", size);
 	fclose (f);
+	*length = size;
+}
+
+void bubbleSort(int *list, int length)
+{
 	int keepSorting = 1;
-	i = 0;
+	int i = 0;
 	int j = 0;
+
 	while(keepSorting != 0)
 	{
-//		printf("outer_while[%d]\n", j);
 		keepSorting = 0;
 		i = 0;
-		while (i < size - 1)
+		while (i < length - 1)
 		{
 			if (list[i] > list[i + 1])
 			{
@@ -47,6 +63,13 @@ int main(int argc, char *argv[])
 		}
 		j++;
 	}
-	printf("\n");
-	printList(list, size);
+}
+
+// Debug only, remove
+void printList(int list[], int length)
+{
+	for (int i = 0; i < length; i ++)
+	{
+		printf("List[%d]=%d\n", i, list[i]);
+	}
 }
