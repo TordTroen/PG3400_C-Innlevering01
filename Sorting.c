55de8168067c5b7b6946e1f94c40c72f518412e8
Tord <assessedgrub@gmail.com>
2015-09-25 08:17:43 -0700

    Added dynamic array

--- Sorting.c
+++ Sorting.c
@@ -2,18 +2,40 @@
 #include<stdlib.h>
 
 // Function declarations
-void printList(int list[], int length);
+void printList(int *list, int length);
 void readFileContent(char *fileName, int *length, int *list);
 void bubbleSort(int *list, int length);
 void searchList(int list[], int length);
 
 int main(int argc, char *argv[])
 {
-	int length = 0; // Length of the list
-	int list[200000]; // The list to be sorted
-
+	int length = 5; // Length of the list
+	//int list[200000]; // The list to be sorted
+	int *list;//, size = 5;
+	list = (int*)malloc(length*sizeof(int));
 	// Read numbers from file to list
-	readFileContent("TestFile.txt", &length, list);
+	//readFileContent("1.txt", &length, list);
+	FILE* f = fopen ("1.txt" , "r");
+	int num = 0;
+	int i = 0;
+	//printf("Size: %d\n", length);
+	while (!feof (f))
+	{
+		if (i >= length - 1)
+		{
+			length *= 2;
+			list = realloc(list, length*sizeof(int));
+			//printf("Reallocing, new size: %d\n", length);
+		}
+		fscanf (f, "%d", &num);
+		list[i] = num;
+		i ++;
+	}
+	fclose (f);
+	length = i;
+	list = realloc(list, length*sizeof(int));
+	printList(list, length);
+	//*length = size;
 	
 	// TODO: take input and chose sorting algorithm
 
@@ -25,20 +47,21 @@ int main(int argc, char *argv[])
 
 	// Searching for int
 	searchList(list, length);
+
+	free(list);
 }
 
 void readFileContent(char *fileName, int *length, int *list)
 {
-	
 	FILE* f = fopen (fileName , "r");
 	int i = 0;
 	int size = 0;
-	while (!feof (f)) // while not EndOfFile
+	while (!feof (f))
 	{
 		fscanf (f, "%d", &i);
 		list[size] = i;
+
 		if (feof(f)) break;
-		//printf("Read[%d]=%d\n", size, i);
 		size ++;
 	}
 	fclose (f);
@@ -71,7 +94,7 @@ void bubbleSort(int *list, int length)
 }
 
 // Debug only, remove
-void printList(int list[], int length)
+void printList(int *list, int length)
 {
 	for (int i = 0; i < length; i ++)
 	{
